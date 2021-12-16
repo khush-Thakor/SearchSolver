772b6c102993ea9e07eaed15e527ed4d4595a0d5
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-12-15 23:26:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+var rowsC = 0;
+var column = 0;
 window.onload = function(){
+    document.getElementById("imageInput").style.display = "none";
     document.getElementById("manualInput").style.display = "none";
+    document.getElementById("imageButton").addEventListener("click",function(){
+        document.getElementById("manualInput").style.display = "none";
+        document.getElementById("imageInput").style.display = "block";
+    });
     document.getElementById("manualButton").addEventListener("click",function(){
         document.getElementById("manualInput").style.display = "block";
+        document.getElementById("imageInput").style.display = "none";
     });
+
 }
